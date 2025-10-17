69407b108e2acc47bcc683e947ca03af8f3fee1f
Laslo76 <162439161+Laslo76@users.noreply.github.com>
2025-10-17 12:22:52 +0300

    First commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static  void main(String[] args) {
+        String firstName = "Vladyslav";
+        String lastName  = "Bogushev";
+        System.out.printf(firstName + " " + lastName);
+
+    }
+}
